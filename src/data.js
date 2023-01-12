31adf6eb10b13d65631eb3d345e43f21d0d3d05e
Rodika <raryku@yahoo.com>
2023-01-11 21:09:00 -0800

    image change

--- src/data.js
+++ src/data.js
@@ -5,9 +5,9 @@ export const data = [
     quantity: 12,
     description: "Hydrating Facial Mask, With Pure Natural Ingredients For Skincare, Moisturize And Smooth Skin, Brightening And Soothing, Beauty Mask For All Skin Type.",
     price: 64.99,
-    image: ["https://images.unsplash.com/photo-1625753783470-ec2ab4efeeec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80", 
-    "https://images.unsplash.com/photo-1619451334792-150fd785ee74?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=701&q=80", 
-    "https://images.unsplash.com/photo-1609357912334-e96886c0212b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIwfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"]
+    image: ["https://sc04.alicdn.com/kf/H9877ffcfbe314e8aab1f45d53e62166a1.jpg", 
+    "https://s.alicdn.com/@sc04/kf/H704db01c7774405399c0344104012d91y.jpg_960x960.jpg", 
+    "https://s.alicdn.com/@sc04/kf/He3b92806ba4b47cbb8a650c1461497bfF.jpg_960x960.jpg"]
 },
 
 {
@@ -16,9 +16,9 @@ export const data = [
     quantity: 18,
     description: "Browse the range of silky cleansing oils. Featuring our best cleansing oils for skin concerns & conditions like dry skin, anti-aging, skin brightening, skin discoloration & more. our best facial oils for normal skin, mature skin, sensitive skin & dehydrated skin. ",
     price: 97,
-    image: ["https://images.unsplash.com/photo-1563804447971-6e113ab80713?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80", 
-    "https://images.unsplash.com/photo-1506003094589-53954a26283f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fGJlYXV0eSUyMHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60", 
-    "https://images.unsplash.com/photo-1564278047230-a632a9d6acf4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDJ8fGJlYXV0eSUyMHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"]
+    image: ["https://img.freepik.com/free-photo/front-view-arrangement-natural-argan-product_23-2148955804.jpg?w=740&t=st=1673499342~exp=1673499942~hmac=6521a6bc0aee6c6e905de745f15f3abf7a956d26220d21393f0a229a589b308c", 
+    "https://img.freepik.com/free-photo/arrangement-skin-beauty-products_23-2148761484.jpg?w=996&t=st=1673499298~exp=1673499898~hmac=a6325fb6517e86d418391a6ef2b8356fab316001d8c962ac0efee99c48b587fd", 
+    "https://img.freepik.com/free-photo/healthy-jojoba-oil-treatment-arrangement_23-2149022415.jpg?w=740&t=st=1673499450~exp=1673500050~hmac=ffaa253c1c0e9a467e53223719f0a5feb07ac8563e20455be0d750c6a3f9e210"]
 },
 
 {
@@ -28,7 +28,7 @@ export const data = [
     description: "Browse makeup products. Featuring face, eye, and lip makeup, including foundation and concealer, mascara and eyeliner, lipstick and liquid lipstick. find the perfect addition to your makeup bag for your creative or natural makeup looks. ",
     price: 178,
     image: ["https://images.unsplash.com/photo-1522338242992-e1a54906a8da?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDZ8fGJlYXV0eSUyMHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60", 
-    "https://images.unsplash.com/photo-1522335789203-aabd1fc54bc9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDR8fGJlYXV0eSUyMHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60", 
+    "https://img.freepik.com/free-photo/high-angle-lipsticks-arrangement_23-2148978114.jpg?w=740&t=st=1673492558~exp=1673493158~hmac=48c35760b31839a96aa1631a035d0e5a692cf132de791205e525b071dac61a65", 
     "https://images.unsplash.com/photo-1547887538-047f814bfb64?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzR8fGJlYXV0eSUyMHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"]
 },
 
@@ -39,8 +39,8 @@ export const data = [
     description: "13 PCS Makeup Brushes Set Eye Shadow Foundation BB Cream Concealer Women Cosmetic Brush Eyeshadow Blush Powder Blending Beauty Soft Comprehensive Make Up Tools.",
     price: 64,
     image: ["https://images.unsplash.com/photo-1620464003286-a5b0d79f32c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80", 
-    "https://images.unsplash.com/photo-1512206879471-b4d119aef899?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60", 
-    "https://images.unsplash.com/photo-1516975080664-ed2fc6a32937?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"]
+    "https://img.freepik.com/free-photo/different-type-makeup-brushes-row_23-2147878869.jpg?w=1480&t=st=1673493746~exp=1673494346~hmac=81bb2b180856cacb759016e823dfa70dab7b676a8de36f0b2a7ee3ebc31f6bd2", 
+    "https://img.freepik.com/free-vector/realistic-make-up-brush-collection_23-2147818508.jpg?w=996&t=st=1673493660~exp=1673494260~hmac=9a15431169a56be3335a07791f0f428df031aaf326896ef7482488c48afd6166"]
 },
 
 {
@@ -49,8 +49,8 @@ export const data = [
     quantity: 19,
     description: "Skincare Products That Boosts Hydration, Improve Skin Surface & Diminish Skin Aging Signs. Diminish Up To 10 Years Of Visible Skin Damage After 1 Year Of PITERA™ Regimen. Award-Winning Skincare.",
     price: 89,
-    image: ["https://images.unsplash.com/photo-1598440947619-2c35fc9aa908?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80", 
-    "https://images.unsplash.com/photo-1597106776019-b4ecc878c202?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60", 
+    image: ["https://img.freepik.com/free-photo/close-up-body-butter-recipient_23-2148543053.jpg?w=740&t=st=1673494077~exp=1673494677~hmac=c933ff9a932e9fe3b2d4b5d2532709fbfca4875477eef61b53ba4faa7ea25b1a", 
+    "https://img.freepik.com/premium-photo/blank-cosmetic-products-with-fir-branches_179738-520.jpg?w=740", 
     "https://images.unsplash.com/photo-1556227834-09f1de7a7d14?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"]
 },
 
@@ -62,6 +62,6 @@ export const data = [
     price: 157,
     image: ["https://images.unsplash.com/photo-1541643600914-78b084683601?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2804&q=80",
     "https://images.unsplash.com/photo-1563170351-be82bc888aa4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
-    "https://images.unsplash.com/photo-1458538977777-0549b2370168?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"]
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5X05oszfLuIc36qfIzaCvwXcK7kUwVCIIwQ&usqp=CAU"]
 }
 ]
